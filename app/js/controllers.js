32336e21c3f78f9d1ac588b92d69f96558a7cb47
Horace Heaven <hheaven@medullan.com>
2013-07-25 15:32:57 -0400

    Added angular mock json data to services and PlannedAbsenceCtrl to controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,14 @@
-'use strict';
+angular.module('awa.controllers', [])
+    .controller('PlannedAbsenceCtrl', function($scope, PlannedAbsenceService) {
+        $scope.dataset = PlannedAbsenceService;
 
-/* Controllers */
+        $scope.calculateDifferenceInDays = function(startDateText, endDateText) {
+            var startDate = new Date(startDateText);
+            var endDate = new Date(endDateText);
 
-angular.module('awa.controllers', []).
-  controller('MyCtrl1', [function() {
+            var timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+            var dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1;
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+            return dayDiff > 1 ? dayDiff + ' Days' : dayDiff + ' Day'
+        }
+    });
